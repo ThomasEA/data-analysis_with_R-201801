c6f12718b86b5ae67b7e3cb6d6f009a974965182
Everton Thomas <everton.a.thomas@gmail.com>
2018-05-24 19:41:02 -0300

    Atividade Aula 04

--- aula-04/02_atividade.R
+++ aula-04/02_atividade.R
@@ -5,6 +5,8 @@ library(lubridate)
 ## Utilize o código daquele exercício como ponto de partida para esta atividade. 
 ## Sempre utilize o caminho relativo, não o caminho absoluto, pois não funcionará na correção do exercício.
 
+salarios <- read_csv("aula-04/data/201802_dados_salarios_servidores.csv.gz")
+
 ### IMPORTANTE ###
 ## Se você utilizar alguma função própria ou do material de aula, o código da(s) função(ões) deve estar neste arquivo da atividade.
 
@@ -20,6 +22,22 @@ library(lubridate)
 ## 
 ### # ####
 
+d <- salarios %>%
+  group_by(DESCRICAO_CARGO) %>%
+  summarise(qtd_serv = n(), 
+            coef_corr = cor(year(Sys.Date()) - year(DATA_INGRESSO_ORGAO), year(Sys.Date()) - year(DATA_DIPLOMA_INGRESSO_SERVICOPUBLICO))) %>%
+  ungroup() %>%
+  filter(qtd_serv >= 200) %>% #filtra os registros
+  arrange(qtd_serv) %>% #ordena
+  mutate(DIR_CORR = ifelse(coef_corr >= 0, 'POSITIVO', 'NEGATIVO')) %>%
+  mutate(FORCA_CORR = ifelse(abs(coef_corr) >= 0.9, 'Muito Forte',
+                             ifelse(abs(coef_corr) >= 0.7 & abs(coef_corr) < 0.9, 'Forte',
+                                    ifelse(abs(coef_corr) >= 0.5 & abs(coef_corr) < 0.7, 'Moderada',
+                                           ifelse(abs(coef_corr) >= 0.3 & abs(coef_corr) < 0.5, 'Fraca',
+                                                  'Desprezível'))))) %>%
+  select(DESCRICAO_CARGO, coef_corr, DIR_CORR, FORCA_CORR)
+
+
 ### 2 ###
 ##
 ## - A partir do dataset do exercício anterior, selecione os 10 cargos de correlação mais forte (seja positiva ou negativa) e os 
@@ -29,4 +47,42 @@ library(lubridate)
 ##   (caso haja diferença)
 ##
 ### # ###
+calcula_moda_lotacao <- function(c) {
+    as.character(r <- salarios %>%
+    filter(salarios$DESCRICAO_CARGO == as.character(c)) %>%
+    count(ORGSUP_LOTACAO) %>%
+    arrange(desc(n)) %>%
+    head(1) %>%
+    select(ORGSUP_LOTACAO))
+}
+
+calcula_moda_exercicio <- function(c) {
+  as.character(r <- salarios %>%
+                 filter(salarios$DESCRICAO_CARGO == as.character(c)) %>%
+                 count(ORGSUP_EXERCICIO) %>%
+                 arrange(desc(n)) %>%
+                 head(1) %>%
+                 select(ORGSUP_EXERCICIO))
+}
+
+dez_mais <- d %>%
+  arrange(desc(abs(coef_corr))) %>%
+  head(10)
+
+dez_menos <- d %>%
+  arrange(abs(coef_corr)) %>%
+  head(10)
+
+records <- rbind(dez_mais, dez_menos) #junta dois datasets
+
+records %>% pull(DESCRICAO_CARGO) -> cargos
+
+resultado_2 <- records %>%
+  group_by(DESCRICAO_CARGO, FORCA_CORR) %>%
+  summarise(MODA_ORGSUP_LOTACAO = calcula_moda_lotacao(DESCRICAO_CARGO),
+            MODA_ORGSUP_EXERCICIO = calcula_moda_exercicio(DESCRICAO_CARGO))
+
+
+#Resposta:
+#Existe diferença entre as Modas. No geral, quando existe diferença a força da correlação tende a ser Desprezíl, para a amostra analisada
 
