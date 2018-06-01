36a9e768a9ade485a74a6a61e4cc55f72d36df26
Kratos <everton.a.thomas@gmail.com>
2018-06-01 06:58:53 -0300

    Atividade ggplot

--- aula-05/atividade_ggplot.R
+++ aula-05/atividade_ggplot.R
@@ -5,3 +5,18 @@ library(lubridate)
 library(magrittr)
 library(Hmisc)
 
+ted_main <- read.csv("aula-05/data/ted_main.csv.gz")
+
+
+
+ted_main %>%
+  mutate(ano_publicacao = year(as_datetime(published_date))) %>%
+  filter(ano_publicacao >= 2012 & ano_publicacao <= 2017) -> ted_main_transf
+  #group_by(event, ano_publicacao) %>%
+  #summarise(num_views = sum(views)) -> ted_main_t
+ggplot(ted_main_transf, aes( x = views)) +
+  geom_histogram(bins = 30) +
+  facet_wrap(~ ano_publicacao, ncol = 3) + 
+  labs( x = 'Total de visualizações'
+        )
+
