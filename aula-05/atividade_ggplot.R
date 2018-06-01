29ee50d07b45860b6a6cf61d827c6d2cdb421c81
Kratos <everton.a.thomas@gmail.com>
2018-06-01 06:29:11 -0300

    Inicio atividade ggplot

---
+++ aula-05/atividade_ggplot.R
@@ -0,0 +1,7 @@
+if (!"Hmisc" %in% installed.packages()) install.packages("Hmisc")
+if (!"ggcorrplot" %in% installed.packages()) install.packages("ggcorrplot")
+library(tidyverse)
+library(lubridate)
+library(magrittr)
+library(Hmisc)
+
